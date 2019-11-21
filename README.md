e353b33cdd1e6cc481924cc92a090340351cf275
benmerchant <benjaminmerchant@gmail.com>
2019-11-20 20:23:36 -0500

    so far so good

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## Vue.js: GEtting Started
 ### 54 Minutes
 #### Expertise Level: Beginner
+
+Developers Note: Yeah, I was lazy with the styling. 
---
+++ main.js
@@ -0,0 +1,42 @@
+/**
+ * November 20, 2019
+ */
+// why did he use [var]???
+
+const app = new Vue({
+  el: '#some-vue-app',
+  data: {
+    title: 'This is Some View App',
+    subtitle: 'This app was made to learn Vue.js more better',
+    foods: [
+      {
+        name: 'apple',
+        description: 'largest company ever',
+        img_url: 'apple.jpg',
+        coolness_factor: 10,
+        is_fav: true
+      },
+      {
+        name: 'dragon',
+        description: 'awesome monster',
+        img_url: 'dragon.jpg',
+        coolness_factor: 20,
+        is_fav: true
+      },
+      {
+        name: 'charlie',
+        description: 'fantastic janitor',
+        img_url: 'charlie.jpg',
+        coolness_factor: 30,
+        is_fav: true
+      },
+      {
+        name: 'pear',
+        description: 'sandy fruit',
+        img_url: 'pear.jpg',
+        coolness_factor: 0,
+        is_fav: false
+      },
+    ]
+  }
+});
